6b9d53afa805cec9d09c02cbd09d664f03a33367
Thomas Cruz <tcruz2994@gmail.com>
2024-09-22 23:47:57 -0500

    moved starship.toml to .config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+## FISH CURSOR SETTING
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual block
+
 ## ENABLE VI BINDINGS
 fish_vi_key_bindings
 

90e5ba4d3bd1f892d8bd9c6f49ab6135465890ca
Thomas Cruz <tcruz2994@gmail.com>
2025-03-15 01:25:25 -0500

    moved to stow format

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,33 @@
+#if status is-interactive
+#    and not set -q TMUX
+#    exec tmux
+#end
+
+## FISH CURSOR SETTING
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+set fish_cursor_external line
+set fish_cursor_visual block
+
+## ENABLE VI BINDINGS
+fish_vi_key_bindings
+
+## DISABLE FISH GREETING
+set fish_greeting
+
+## ENABLE STARSHIP PROMPT
+set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
+starship init fish | source
+
+## ALIASES
+source ~/.config/.aliases
+
+## VARIABLES
+
+## PATH EXPORTS
+fish_add_path /opt/homebrew/bin
+
+## EXTRAS
+neofetch

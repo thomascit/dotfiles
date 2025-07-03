84f0e883f804a0b9adc06b8232ca639a5a65e4a6
Thomas Cruz <tcruz2994@gmail.com>
2025-07-02 23:02:11 -0500

    cleaning!

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
-## THEME
+# THEME
 fish_config theme choose "Dracula Official"
 
-## FISH CURSOR SETTING
+# FISH CURSOR SETTING
 set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
@@ -9,24 +9,23 @@ set fish_cursor_replace underscore
 set fish_cursor_external line
 set fish_cursor_visual block
 
-## ENABLE VI BINDINGS
+# ENABLE VI BINDINGS
 fish_vi_key_bindings
 
-## DISABLE FISH GREETING
+# DISABLE FISH GREETING
 set fish_greeting
 
-## ALIASES
 source ~/.config/aliases
 
-## PATH EXPORTS
-fish_add_path /opt/homebrew/bin .local/bin
+# PATH EXPORTS
+fish_add_path /opt/homebrew/bin ~/.local/bin
 
-## ENABLE STARSHIP PROMPT
+# ENABLE STARSHIP PROMPT
 set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
 starship init fish | source
 
-## ENABLE ZOXIDE
+# ENABLE ZOXIDE
 zoxide init fish | source
 
-## EXTRAS
+# EXTRAS
 fastfetch

89dd50632eb8a4bf0c567597852fac1d1c03b274
Thomas Cruz <tcruz2994@gmail.com>
2025-09-06 15:15:10 -0500

    added fzf

---
+++ fzf/.config/fzfrc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# fzf configuration shared across shells
+
+# Default command
+export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*"'
+
+# Default theme/colors (Dracula)
+export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+
+# ctrl-t: file completion with preview via bat
+export FZF_CTRL_T_OPTS="
+  --multi
+  --reverse
+  --walker-skip .git
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+# ctrl-r: history search
+export FZF_CTRL_R_OPTS="
+  --multi
+  --reverse"
+
+# alt-c: cd into directories
+export FZF_ALT_C_OPTS="
+  --multi
+  --reverse
+  --walker-skip .*"

4e0a2ed52b58f806cdd82ae71b9e9ca651b8e713
thomascit <tcruz2994@gmail.com>
2025-09-08 13:45:22 -0500

    dracula theme for fzf

--- fzf/.config/fzfrc.fish
+++ fzf/.config/fzfrc.fish
@@ -1,8 +1,11 @@
 #!/usr/bin/env fish
 # fzf configuration for Fish shell
 
+# Default command
+set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git/*"'
+
 # Default theme/colors (Dracula)
-set -gx FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+set -gx FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --inline-info"
 
 # ctrl-t: file completion with preview via bat
 set -gx FZF_CTRL_T_OPTS "
@@ -14,11 +17,10 @@ set -gx FZF_CTRL_T_OPTS "
 
 # ctrl-r: history search
 set -gx FZF_CTRL_R_OPTS "
-  --multi
   --reverse"
 
 # alt-c: cd into directories
 set -gx FZF_ALT_C_OPTS "
-  --multi
   --reverse"
 
+set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git/*"'

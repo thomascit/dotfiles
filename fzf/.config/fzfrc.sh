3ccbbd7837aa3406db970c59c3bfc5b1e9d28a0c
Thomas Cruz <tcruz2994@gmail.com>
2025-09-08 13:45:22 -0500

    dracula theme for fzf

--- fzf/.config/fzfrc.sh
+++ fzf/.config/fzfrc.sh
@@ -2,10 +2,10 @@
 # fzf configuration shared across shells
 
 # Default command
-export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --ignore .git -l'
 
 # Default theme/colors (Dracula)
-export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --layout=reverse'
 
 # ctrl-t: file completion with preview via bat
 export FZF_CTRL_T_OPTS="

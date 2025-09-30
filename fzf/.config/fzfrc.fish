eb5576e5dc3cbad384ee1824e179553408a5b0cc
thomascit <tcruz2994@gmail.com>
2025-09-29 19:34:14 -0500

    fzf default empty fix

--- fzf/.config/fzfrc.fish
+++ fzf/.config/fzfrc.fish
@@ -2,7 +2,7 @@
 # fzf configuration for Fish shell
 
 # Default command
-set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden'
+set -gx FZF_DEFAULT_COMMAND 'rg --files; or true'
 
 # Default theme/colors (Dracula)
 set -gx FZF_DEFAULT_OPTS '--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --layout=reverse'

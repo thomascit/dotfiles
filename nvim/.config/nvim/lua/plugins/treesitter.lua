dc65f5cb1820d1e594f4e87b1258dec7fff21a06
thomascit <tcruz2994@gmail.com>
2025-10-06 17:16:08 -0500

    new plugins (still work-in-progress)

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  build = ":TSUpdate", -- Ensures parsers are updated on plugin updates
   config = function()
     require("nvim-treesitter.configs").setup({
-      -- Add languages to be installed here
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "javascript",
-        "typescript",
-        "python",
-        "html",
-        "css",
-        "json",
-        "markdown",
-        "markdown_inline",
-      },
-      
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-      
-      -- Automatically install missing parsers when entering buffer
-      auto_install = true,
-      
-      highlight = {
-        enable = true,
-        -- Disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-      },
-      
-      indent = {
-        enable = true,
-      },
+      -- Global configuration options for treesitter
+      highlight = { enable = true }, -- Enable syntax highlighting
+      indent = { enable = true },   -- Enable indentation
+      ensure_installed = { "lua", "vim", "python" }, -- Example languages to install
+      auto_install = true, -- Automatically install missing parsers on buffer open
+      -- Further configurations can be added here
     })
   end,
 }

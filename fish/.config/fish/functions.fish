8c5ea062894b77f5075a460fe6ad4df8ec1fb7b2
thomascit <tcruz2994@gmail.com>
2025-11-11 23:12:57 -0600

    new fish structure

---
+++ fish/.config/fish/functions.fish
@@ -0,0 +1,21 @@
+# Yazi Shell wrapper
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
+# Lazygit wrapper function
+function lg
+    set -gx LAZYGIT_NEW_DIR_FILE ~/.lazygit/newdir
+
+    lazygit $argv
+
+    if test -f $LAZYGIT_NEW_DIR_FILE
+        cd (cat $LAZYGIT_NEW_DIR_FILE)
+        rm -f $LAZYGIT_NEW_DIR_FILE >/dev/null
+    end
+end

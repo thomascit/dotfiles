6dd521f1ee568b2e0c8eed771185954f601536db
thomascit <tcruz2994@gmail.com>
2026-01-06 18:47:07 -0600

    removed yatline

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,63 +1,12 @@
 -- Plugins
-local dracula_theme = require("yatline-dracula"):setup()
 
-require("zoxide"):setup {
+require("zoxide"):setup({
 	update_db = false,
-}
+})
 
-
-require("full-border"):setup {
+require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.PLAIN,
-}
+})
 
 require("git"):setup()
-
-require("yatline"):setup({
-	show_background = true,
-    theme = dracula_theme,
-	header_line = {
-		left = {
-			section_a = {
-        			{type = "line", custom = false, name = "tabs", params = {"left"}},
-			},
-			section_b = {
-			},
-			section_c = {
-			}
-		},
-		right = {
-			section_a = {
-			},
-			section_b = {
-			},
-			section_c = {
-        			{type = "coloreds", custom = false, name = "count"},
-			}
-		}
-	},
-	status_line = {
-		left = {
-			section_a = {
-        			{type = "string", custom = false, name = "tab_mode"},
-			},
-			section_b = {
-        			{type = "string", custom = false, name = "hovered_size"},
-			},
-			section_c = {
-        			{type = "string", custom = false, name = "hovered_name"},
-			}
-		},
-		right = {
-			section_a = {
-        			{type = "string", custom = false, name = "cursor_position"},
-			},
-			section_b = {
-        			{type = "string", custom = false, name = "cursor_percentage"},
-			},
-			section_c = {
-        			{type = "coloreds", custom = false, name = "permissions"},
-			}
-		}
-	},
-})

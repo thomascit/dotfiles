3bfd59b2539276e2f8134bd09adf12f8fe7b7e8f
Thomas Cruz <tcruz2994@gmail.com>
2026-01-13 12:21:30 -0600

    Add Linux window manager configs (hypr, i3, polybar, waybar)

---
+++ i3/.config/i3/lock.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#00000000'
+DEFAULT='#6272A4'
+TEXT='#8BE9FD'
+WRONG='#FF5555'
+VERIFYING='FF79C6'
+RIGHT='#50FA7B'
+
+i3lock \
+  --insidever-color=$CLEAR \
+  --ringver-color=$VERIFYING \
+  \
+  --insidewrong-color=$CLEAR \
+  --ringwrong-color=$WRONG \
+  \
+  --inside-color=$BLANK \
+  --ring-color=$DEFAULT \
+  --line-color=$BLANK \
+  --separator-color=$DEFAULT \
+  \
+  --verif-color=$TEXT \
+  --wrong-color=$TEXT \
+  --time-color=$TEXT \
+  --date-color=$TEXT \
+  --keyhl-color=$RIGHT \
+  --bshl-color=$WRONG \
+  \
+  --screen 1 \
+  --blur 5 \
+  --clock \
+  --indicator \
+  --time-str="%I:%M %p" \
+  --date-str="%m-%d-%Y"

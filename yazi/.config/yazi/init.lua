f511ca73207193865f385c9ac98909512d9dcddd
thomascit <tcruz2994@gmail.com>
2026-02-21 15:57:47 -0600

    reinstalled yatline with new dracula overrides

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,12 +1,75 @@
 -- Plugins
 
-require("zoxide"):setup({
-	update_db = false,
-})
-
 require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.PLAIN,
 })
 
 require("git"):setup()
+
+local dracula_theme = require("yatline-dracula"):setup()
+require("yatline"):setup({
+	theme = dracula_theme,
+	-- Theme Overrides
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
+	selected = { icon = "󰻭", fg = "#f1fa8c" },
+	-- Defaults
+	padding = { inner = 1, outer = 1 },
+
+	tab_width = 20,
+
+	show_background = true,
+
+	display_header_line = true,
+	display_status_line = true,
+
+	component_positions = { "header", "tab", "status" },
+
+	header_line = {
+		left = {
+			section_a = {
+				{ type = "line", name = "tabs" },
+			},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {
+				{ type = "string", name = "date", params = { "%A, %d %B %Y" } },
+			},
+			section_b = {
+				{ type = "string", name = "date", params = { "%X" } },
+			},
+			section_c = {},
+		},
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", name = "hovered_path" },
+				{ type = "coloreds", name = "count" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string", name = "hovered_file_extension", params = { true } },
+				{ type = "coloreds", name = "permissions" },
+			},
+		},
+	},
+})

5408af38387b47d15f2aa888f084155fd4447671
Thomas Cruz <tcruz2994@gmail.com>
2026-02-25 11:41:54 -0600

    added cutlass

---
+++ nvim/.config/nvim/lua/plugins/cutlass.lua
@@ -0,0 +1,9 @@
+-- Lua
+return {
+  "gbprod/cutlass.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or don't set opts to use the default settings
+    -- refer to the configuration section below
+  },
+}

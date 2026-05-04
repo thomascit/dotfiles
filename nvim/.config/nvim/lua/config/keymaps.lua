be3a32daca6ba9e320cc45efdc101db896076786
thomascit <tcruz2994@gmail.com>
2026-05-03 23:15:16 -0500

    new y keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,10 +2,13 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local map = vim.keymap.set 
+local map = vim.keymap.set
 
 -- Insert mode mappings
 map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+-- Keep highlight after yank
+map("v", "y", "ygv", { noremap = true })
 
 -- Wrap mappings
 map("n", "<leader>tw", function()
@@ -15,6 +18,7 @@ map("n", "<leader>tw", function()
   vim.wo.breakindent = not on
 end, { desc = "Wrap" })
 
+-- Colorcolumn
 map("n", "<leader>tc", function()
   local has_column = vim.wo.colorcolumn ~= ""
   vim.wo.colorcolumn = has_column and "" or "80"
@@ -22,9 +26,15 @@ end, { desc = "Colorcolumn" })
 
 -- Quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
-map("n", "<leader>qb", function() Snacks.bufdelete() end, { desc = "Quit Buffer" })
-map("n", "<leader>qo", function() Snacks.bufdelete.other() end, { desc = "Quit Other Buffers" })
-map("n", "<leader>qa", function() Snacks.bufdelete.all() end, { desc = "Quit All Buffers" })
+map("n", "<leader>qb", function()
+  Snacks.bufdelete()
+end, { desc = "Quit Buffer" })
+map("n", "<leader>qo", function()
+  Snacks.bufdelete.other()
+end, { desc = "Quit Other Buffers" })
+map("n", "<leader>qa", function()
+  Snacks.bufdelete.all()
+end, { desc = "Quit All Buffers" })
 
 -- Save
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
@@ -37,17 +47,36 @@ map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
 -- lazygit
 if vim.fn.executable("lazygit") == 1 then
-  map("n", "<leader>gg", function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" })
-  map("n", "<leader>gG", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
+  map("n", "<leader>gg", function()
+    Snacks.lazygit({ cwd = LazyVim.root.git() })
+  end, { desc = "Lazygit (Root Dir)" })
+  map("n", "<leader>gG", function()
+    Snacks.lazygit()
+  end, { desc = "Lazygit (cwd)" })
 end
 
-map("n", "<leader>gL", function() Snacks.picker.git_log() end, { desc = "Git Log (cwd)" })
-map("n", "<leader>gb", function() Snacks.picker.git_log_line() end, { desc = "Git Blame Line" })
-map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Current File History" })
-map("n", "<leader>gl", function() Snacks.picker.git_log({ cwd = LazyVim.root.git() }) end, { desc = "Git Log" })
-map({ "n", "x" }, "<leader>gB", function() Snacks.gitbrowse() end, { desc = "Git Browse (open)" })
-map({"n", "x" }, "<leader>gY", function()
-  Snacks.gitbrowse({ open = function(url) vim.fn.setreg("+", url) end, notify = false })
+map("n", "<leader>gL", function()
+  Snacks.picker.git_log()
+end, { desc = "Git Log (cwd)" })
+map("n", "<leader>gb", function()
+  Snacks.picker.git_log_line()
+end, { desc = "Git Blame Line" })
+map("n", "<leader>gf", function()
+  Snacks.picker.git_log_file()
+end, { desc = "Git Current File History" })
+map("n", "<leader>gl", function()
+  Snacks.picker.git_log({ cwd = LazyVim.root.git() })
+end, { desc = "Git Log" })
+map({ "n", "x" }, "<leader>gB", function()
+  Snacks.gitbrowse()
+end, { desc = "Git Browse (open)" })
+map({ "n", "x" }, "<leader>gY", function()
+  Snacks.gitbrowse({
+    open = function(url)
+      vim.fn.setreg("+", url)
+    end,
+    notify = false,
+  })
 end, { desc = "Git Browse (copy)" })
 
 -- Format
@@ -61,7 +90,9 @@ map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlights" })
 -- Buffer navigation
 map("n", "H", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "L", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "<leader>bd", function() Snacks.bufdelete() end, { desc = "Delete Buffer" })
+map("n", "<leader>bd", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
 
 -- Centered scrolling
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down (centered)" })

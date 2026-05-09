43002acaab0b16b6c4670466ef803c8d2125550c
thomascit <tcruz2994@gmail.com>
2026-05-08 19:59:30 -0500

    nvim: migrate treesitter to opts table and expand parser list

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate", -- Ensures parsers are updated on plugin updates
-  config = function()
-    require("nvim-treesitter").setup({
-      -- Global configuration options for treesitter
-      highlight = { enable = true }, -- Enable syntax highlighting
-      indent = { enable = true },   -- Enable indentation
-      ensure_installed = { "lua", "vim", "python" }, -- Example languages to install
-      auto_install = true, -- Automatically install missing parsers on buffer open
-      -- Further configurations can be added here
-    })
-  end,
+  branch = "main",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "lua", "vim", "vimdoc", "python", "markdown", "markdown_inline", "yaml" },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }

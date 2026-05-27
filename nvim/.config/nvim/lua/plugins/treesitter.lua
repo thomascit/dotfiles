d70cffeacc4200c1169b0e770395b2e034960eee
thomascit <tcruz2994@gmail.com>
2026-05-27 10:18:39 -0500

    treesitter cleanup

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "main",
   build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
   opts = {
-    ensure_installed = { "lua", "vim", "vimdoc", "python", "markdown", "markdown_inline", "yaml" },
-    auto_install = true,
+    ensure_installed = {
+      "bash",
+      "comment",
+      "dockerfile",
+      "go",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "yaml",
+    },
     highlight = { enable = true },
     indent = { enable = true },
   },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }

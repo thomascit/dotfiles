5d794770371ad0fdae60662b696a0eb8f242a6bc
thomascit <tcruz2994@gmail.com>
2026-05-31 13:56:10 -0500

    mini sh icon

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -2,8 +2,22 @@ return {
   'nvim-mini/mini.nvim',
   version = false,
   config = function()
-        require('mini.ai').setup()
-        require('mini.pairs').setup()
+    require('mini.ai').setup()
+    require('mini.icons').setup({
+      filetype = {
+        -- Use console icon and green color for shell scripts
+        sh = { glyph = '󰆍', hl = 'MiniIconsGreen' },
+      },
+    })
+    require('mini.pairs').setup()
+
+    -- Defer mini.trailspace setup until user opens a real file
+    -- This prevents red blocks on dashboard ASCII art at startup
+    vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+      once = true,
+      callback = function()
         require('mini.trailspace').setup()
-  end
+      end,
+    })
+  end,
 }
